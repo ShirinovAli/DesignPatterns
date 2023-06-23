1a7890e4cd137f4f0ae6e8c8d4a721bc74ce3b7b
ali-shirinov@hotmail.com <ali-shirinov@hotmail.com>
2023-06-23 12:03:16 +0400

    Add project files.

---
+++ BaseIdentityProject/DataAccess/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using BaseIdentityProject.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaseIdentityProject.DataAccess
+{
+    public class ApplicationContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ StrategyPattern.WebApp/DataAccess/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StrategyPattern.WebApp.Models;
+
+namespace StrategyPattern.WebApp.DataAccess
+{
+    public class ApplicationContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+
+        }
+    }
+}
